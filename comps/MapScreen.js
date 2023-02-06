11d3f5c346ac40e7df4d9485b5794a9f0680a8fe
DeeAnnEye <danielashulamithjustin@gmail.com>
2023-02-06 23:17:10 +0530

    add initialCamera

--- comps/MapScreen.js
+++ comps/MapScreen.js
@@ -1,4 +1,4 @@
-import React , {useState,useEffect} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   NativeBaseProvider,
   Input,
@@ -13,41 +13,49 @@ import {
 import { StyleSheet } from "react-native";
 import MapView, { Marker, AnimatedRegion } from "react-native-maps";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import * as Location from 'expo-location';
+import * as Location from "expo-location";
 
 const MapScreen = () => {
-
+  const map = useRef();
   const [location, setLocation] = useState(null);
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
 
   useEffect(() => {
     (async () => {
-      
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
         return;
       }
 
-      let loc = await Location.getCurrentPositionAsync({});      
+      let loc = await Location.getCurrentPositionAsync({});
       setLocation(loc);
-      setLatitude(loc.coords.latitude)
-      setLongitude(loc.coords.longitude)
     })();
   }, []);
+
+  const initialCamera = {
+    center: {
+      latitude: 10.0618591,
+      longitude: 76.2971976,
+    },
+    pitch: 45,
+    heading: 0,
+    altitude: 0,
+    zoom: 10,
+    type: "standard",
+  };
 
   return (
     <NativeBaseProvider>
       <Container h="100%" w="100%" maxWidth="100%" bg="coolGray.50">
         <MapView
+          ref={map}
           followUserLocation={true}
           zoomEnabled={true}
+          initialCamera={initialCamera}
           showsUserLocation={true}
           style={{ width: "100%", height: "100%" }}
           provider={MapView.PROVIDER_GOOGLE}
         />
-         
       </Container>
     </NativeBaseProvider>
   );
